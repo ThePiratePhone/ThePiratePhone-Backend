2108d99cc1f4c8b580c39aa1dce28548490433cc
UnelDev <enee.prog@gmail.com>
2024-01-25 11:11:01 +0100

    Create listClient.ts

---
+++ router/listClient.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { log } from '../tools/log';
+import { Area } from '../Models/area';
+import { Client } from '../Models/Client';
+
+export default async function listClient(req: Request<any>, res: Response<any>) {
+	const ip = req.socket?.remoteAddress?.split(':').pop();
+	if (
+		!req.body ||
+		typeof req.body.adminCode != 'string' ||
+		(req.body.skip && typeof req.body.skip != 'number') ||
+		(req.body.limit && typeof req.body.limit != 'number')
+	) {
+		res.status(400).send({ message: 'Missing parameters', OK: false });
+		log('Missing parameters from: ' + ip, 'WARNING', 'listClient.ts');
+		return;
+	}
+
+	const area = await Area.findOne({ AdminPassword: req.body.adminCode });
+	if (!area) {
+		res.status(401).send({ message: 'Wrong admin code', OK: false });
+		log('Wrong admin code from ' + ip, 'WARNING', 'listClient.ts');
+		return;
+	}
+	const users = await Client.find({ area: area._id })
+		.skip(req.body.skip ? req.body.skip : 0)
+		.limit(req.body.limit ? req.body.limit : 50);
+	res.status(200).send({ message: 'OK', OK: true, data: { users: users } });
+	log('client list send to ' + ip, 'INFORMATION', 'listClient.ts');
+}
