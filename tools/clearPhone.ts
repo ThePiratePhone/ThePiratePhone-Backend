54fdbd9cc669c47f66f23558a785524c789da9e3
Wiwok <Emil.dechenaud@gmail.com>
2024-03-13 16:19:30 +0100

    Optimizing number cleaning

--- tools/clearPhone.ts
+++ tools/clearPhone.ts
@@ -1,18 +1,12 @@
 export default function clearPhone(phoneNumber: string): string {
 	if (typeof phoneNumber != 'string') return '';
 	phoneNumber = phoneNumber.trim();
-	if (phoneNumber.includes(' ')) {
-		phoneNumber = phoneNumber.replaceAll(' ', '');
-	}
-	if (phoneNumber.includes('-')) {
-		phoneNumber = phoneNumber.replaceAll('-', '');
-	}
-	if (phoneNumber.includes('(')) {
-		phoneNumber = phoneNumber.replaceAll('(', '');
-	}
-	if (phoneNumber.includes(')')) {
-		phoneNumber = phoneNumber.replaceAll(')', '');
-	}
+
+	phoneNumber = phoneNumber.replaceAll(' ', '');
+	phoneNumber = phoneNumber.replaceAll('-', '');
+	phoneNumber = phoneNumber.replaceAll('(', '');
+	phoneNumber = phoneNumber.replaceAll(')', '');
+
 	if (phoneNumber.startsWith('0')) {
 		phoneNumber = phoneNumber.replace('0', '+33');
 	}
