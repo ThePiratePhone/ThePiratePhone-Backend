3a837b9f61ec2946963c3c68d18e70cadae59a2a
UnelDev <enee.prog@gmail.com>
2024-03-16 18:43:27 +0100

    merge callHoursStart and callHoursEnd in callHours

--- router/admin/campaign/changeCallHoursEnd.ts
+++ router/admin/campaign/changeCallHoursEnd.ts
@@ -6,24 +6,25 @@ import { Campaign } from '../../../Models/Campaign';
 import getCurrentCampaign from '../../../tools/getCurrentCampaign';
 import { log } from '../../../tools/log';
 
-export default async function changeCallHoursEnd(req: Request<any>, res: Response<any>) {
+export default async function changeCallHours(req: Request<any>, res: Response<any>) {
 	const ip = req.socket?.remoteAddress?.split(':').pop();
 	if (
 		!req.body ||
 		typeof req.body.adminCode != 'string' ||
 		typeof req.body.newEndHours != 'string' ||
+		typeof req.body.newStartHours != 'string' ||
 		!ObjectId.isValid(req.body.area) ||
 		(req.body.CampaignId && !ObjectId.isValid(req.body.CampaignId))
 	) {
 		res.status(400).send({ message: 'Missing parameters', OK: false });
-		log(`Missing parameters from ` + ip, 'WARNING', 'changeCallHoursEnd.ts');
+		log(`Missing parameters from ` + ip, 'WARNING', 'changeCallHours.ts');
 		return;
 	}
 
 	const area = await Area.findOne({ _id: req.body.area, AdminPassword: req.body.adminCode });
 	if (!area) {
 		res.status(401).send({ message: 'Wrong admin code', OK: false });
-		log(`Wrong admin code from ${ip}`, 'WARNING', 'changeCallHoursEnd.ts');
+		log(`Wrong admin code from ${ip}`, 'WARNING', 'changeCallHours.ts');
 		return;
 	}
 
@@ -36,24 +37,33 @@ export default async function changeCallHoursEnd(req: Request<any>, res: Respons
 	}
 	if (!campaign) {
 		res.status(401).send({ message: 'Wrong campaign id', OK: false });
-		log(`Wrong campaign id from ${area.name} (${ip})`, 'WARNING', 'changeCallHoursEnd.ts');
+		log(`Wrong campaign id from ${area.name} (${ip})`, 'WARNING', 'changeCallHours.ts');
 		return;
 	}
 
-	const dateTest = Date.parse(req.body.newEndHours);
-	if (!dateTest) {
-		res.status(400).send({ message: 'Invalid date', OK: false });
-		log(`Invalid date from ${ip}`, 'WARNING', 'changeCallHoursEnd.ts');
+	const dateEnd = Date.parse(req.body.newEndHours);
+	if (!dateEnd) {
+		res.status(400).send({ message: 'Invalid end date', OK: false });
+		log(`Invalid end date from ${ip}`, 'WARNING', 'changeCallHours.ts');
+		return;
+	}
+	const dateStart = Date.parse(req.body.newStartHours);
+	if (!dateStart) {
+		res.status(400).send({ message: 'Invalid start date', OK: false });
+		log(`Invalid start date from ${ip}`, 'WARNING', 'changeCallHours.ts');
 		return;
 	}
 
-	const output = await Campaign.updateOne({ _id: campaign._id }, { callHoursEnd: dateTest });
+	const output = await Campaign.updateOne(
+		{ _id: campaign._id },
+		{ callHoursEnd: dateEnd, callHoursStart: dateStart }
+	);
 	if (output.matchedCount != 1) {
 		res.status(400).send({ message: 'Campaign not found', OK: false });
-		log(`Campaign not found from ${ip}`, 'WARNING', 'changeCallHoursEnd.ts');
+		log(`Campaign not found from ${ip}`, 'WARNING', 'changeCallHours.ts');
 		return;
 	}
 
 	res.status(200).send({ message: 'OK', OK: true });
-	log(`Campaign hours end changed from ${ip} (${area.name})`, 'INFORMATION', 'changeCallHoursEnd.ts');
+	log(`Campaign hours changed from ${ip} (${area.name})`, 'INFORMATION', 'changeCallHours.ts');
 }
