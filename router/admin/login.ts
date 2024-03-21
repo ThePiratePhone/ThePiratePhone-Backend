320c9ecd5588778e95369869522c950af2f7e669
unel <enee.prog@gmail.com>
2024-03-21 15:54:05 +0100

    add data for admin/login

--- router/admin/login.ts
+++ router/admin/login.ts
@@ -3,20 +3,39 @@ import { ObjectId } from 'mongodb';
 
 import { Area } from '../../Models/Area';
 import { log } from '../../tools/log';
+import getCurrentCampaign from '../../tools/getCurrentCampaign';
 
-export default async function exportCallerCsv(req: Request<any>, res: Response<any>) {
+export default async function login(req: Request<any>, res: Response<any>) {
 	const ip = req.socket?.remoteAddress?.split(':').pop();
 	if (!req.body || typeof req.body.adminCode != 'string' || !ObjectId.isValid(req.body.area)) {
 		res.status(400).send({ message: 'Missing parameters', OK: false });
-		log(`Missing parameters from ` + ip, 'WARNING', 'exportCallerCsv.ts');
+		log(`Missing parameters from ` + ip, 'WARNING', 'login.ts');
 		return;
 	}
 
-	if (!(await Area.findOne({ _id: req.body.area, AdminPassword: req.body.adminCode }))) {
+	const area = await Area.findOne({ _id: req.body.area, AdminPassword: req.body.adminCode });
+	if (!area) {
 		res.status(401).send({ message: 'Wrong admin code', OK: false });
-		log(`Wrong admin code from ${ip}`, 'WARNING', 'exportCallerCsv.ts');
+		log(`Wrong admin code from ${ip}`, 'WARNING', 'login.ts');
 		return;
 	}
 
-	res.status(200).send({ message: 'OK', OK: true });
+	let campaign = (await getCurrentCampaign(area._id)) as any;
+	if (!campaign) {
+		res.status(401).send({ message: 'Wrong campaign id', OK: false });
+		log(`Wrong campaign id from ${area.name} (${ip})`, 'WARNING', 'login.ts');
+		return;
+	}
+
+	res.status(200).send({
+		message: 'OK',
+		Data: {
+			actualCampaignName: campaign.name,
+			actualCampaignCallStart: campaign.callHoursStart,
+			actualCampaignCallEnd: campaign.callHoursEnd,
+			actualCampaignMaxCall: campaign.nbMaxCallCampaign,
+			actualCampaignTimeBetweenCall: campaign.timeBetweenCall
+		},
+		OK: true
+	});
 }
