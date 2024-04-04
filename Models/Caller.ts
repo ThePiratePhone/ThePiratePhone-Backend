bd1187c75136c25fd4058786cf691f35e029e09e
UnelDev <enee.prog@gmail.com>
2024-04-04 15:30:55 +0200

    fix schema

--- Models/Caller.ts
+++ Models/Caller.ts
@@ -25,7 +25,7 @@ const CallerSchema = new mongoose.Schema({
 			{
 				date: { type: Date, require: true },
 				client: { type: mongoose.Schema.ObjectId, ref: 'Client', required: true },
-				time: { type: Number, required: true },
+				time: { type: Number, required: false },
 				campaign: { type: mongoose.Schema.ObjectId, ref: 'Campaign', required: true }
 			}
 		]
