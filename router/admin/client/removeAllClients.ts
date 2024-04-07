1e63878c29f767898807179dd798941e6b5dc0a1
unel <enee.prog@gmail.com>
2024-04-07 18:04:46 +0200

    fix remove client and rempve all clients

--- router/admin/client/removeAllClients.ts
+++ router/admin/client/removeAllClients.ts
@@ -3,10 +3,17 @@ import { ObjectId } from 'mongodb';
 import { log } from '../../../tools/log';
 import { Area } from '../../../Models/Area';
 import { Client } from '../../../Models/Client';
+import { Campaign } from '../../../Models/Campaign';
+import getCurrentCampaign from '../../../tools/getCurrentCampaign';
 
 export default async function removeAllClients(req: Request<any>, res: Response<any>) {
 	const ip = req.socket?.remoteAddress?.split(':').pop();
-	if (!req.body || typeof req.body.adminCode != 'string' || !ObjectId.isValid(req.body.area)) {
+	if (
+		!req.body ||
+		typeof req.body.adminCode != 'string' ||
+		!ObjectId.isValid(req.body.area) ||
+		(req.body.CampaignId && !ObjectId.isValid(req.body.CampaignId))
+	) {
 		res.status(400).send({ message: 'Missing parameters', OK: false });
 		log(`Missing parameters from ` + ip, 'WARNING', 'removeAllClients.ts');
 		return;
@@ -19,12 +26,36 @@ export default async function removeAllClients(req: Request<any>, res: Response<
 		return;
 	}
 
-	try {
-		await Client.deleteMany({ area: req.body.area });
-	} catch (e) {
-		res.status(500).send({ message: 'Database error', OK: false });
-		log(`Database error from ${ip}`, 'ERROR', 'removeAllClients.ts');
+	let campaign: InstanceType<typeof Campaign> | null = null;
+
+	if (req.body.CampaignId) {
+		campaign = await Campaign.findOne({ _id: req.body.CampaignId, Area: area._id });
+	} else {
+		campaign = await getCurrentCampaign(area._id);
+	}
+	if (!campaign) {
+		res.status(401).send({ message: 'Wrong campaign id', OK: false });
+		log(`Wrong campaign id from ${area.name} (${ip})`, 'WARNING', 'removeAllClients.ts');
 		return;
+	}
+
+	const client = (await Client.find({
+		['data.' + campaign._id]: { $ne: [] },
+		area: area._id
+	})) as unknown as InstanceType<typeof Client>[];
+
+	if (client.length) {
+		await Promise.all(
+			client.map(async el => {
+				console.log(campaign._id.toString());
+				el.data.delete(campaign._id.toString());
+				if (el.data.size == 0) {
+					await Client.deleteOne(el._id);
+				} else {
+					await el.save();
+				}
+			})
+		);
 	}
 	res.status(200).send({ message: 'OK', OK: true });
 	log(`Clients removed from ${ip} (${area.name})`, 'INFORMATION', 'removeAllClients.ts');
--- router/admin/client/removeClient.ts
+++ router/admin/client/removeClient.ts
@@ -5,6 +5,8 @@ import { Area } from '../../../Models/Area';
 import { Client } from '../../../Models/Client';
 import clearPhone from '../../../tools/clearPhone';
 import phoneNumberCheck from '../../../tools/phoneNumberCheck';
+import { Campaign } from '../../../Models/Campaign';
+import getCurrentCampaign from '../../../tools/getCurrentCampaign';
 
 export default async function removeClient(req: Request<any>, res: Response<any>) {
 	const ip = req.socket?.remoteAddress?.split(':').pop();
@@ -12,7 +14,8 @@ export default async function removeClient(req: Request<any>, res: Response<any>
 		!req.body ||
 		typeof req.body.phone != 'string' ||
 		typeof req.body.adminCode != 'string' ||
-		!ObjectId.isValid(req.body.area)
+		!ObjectId.isValid(req.body.area) ||
+		(req.body.CampaignId && !ObjectId.isValid(req.body.CampaignId))
 	) {
 		res.status(400).send({ message: 'Missing parameters', OK: false });
 		log(`Missing parameters from ` + ip, 'WARNING', 'removeClient.ts');
@@ -33,13 +36,33 @@ export default async function removeClient(req: Request<any>, res: Response<any>
 		return;
 	}
 
-	const output = await Client.deleteOne({ phone: req.body.phone });
-	if (output.deletedCount != 1) {
+	let campaign: InstanceType<typeof Campaign> | null = null;
+
+	if (req.body.CampaignId) {
+		campaign = await Campaign.findOne({ _id: req.body.CampaignId, Area: area._id });
+	} else {
+		campaign = await getCurrentCampaign(area._id);
+	}
+	if (!campaign) {
+		res.status(401).send({ message: 'Wrong campaign id', OK: false });
+		log(`Wrong campaign id from ${area.name} (${ip})`, 'WARNING', 'removeClient.ts');
+		return;
+	}
+
+	const output = await Client.findOne({ phone: req.body.phone, area: area._id });
+	if (!output) {
 		res.status(404).send({ message: 'Client not found', OK: false });
 		log(`Client not found from ${area.name} (${ip})`, 'WARNING', 'removeClient.ts');
 		return;
 	}
-
+	if (output.data.has(campaign._id.toString())) {
+		output.data.delete(campaign._id.toString());
+		if (output.data.size == 0) {
+			Client.deleteOne(output._id);
+		} else {
+			await output.save();
+		}
+	}
 	res.status(200).send({ message: 'OK', OK: true });
 	log(`Client removed from ${ip} (${area.name})`, 'INFORMATION', 'removeClient.ts');
 }
