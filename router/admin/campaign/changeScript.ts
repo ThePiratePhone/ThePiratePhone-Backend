c99668e550222a7be97e97c7612284a4987afd99
unel <enee.prog@gmail.com>
2024-11-02 14:19:04 +0100

    add tests for changeScript

--- router/admin/campaign/changeScript.ts
+++ router/admin/campaign/changeScript.ts
@@ -1,25 +1,50 @@
 import { Request, Response } from 'express';
-import { ObjectId } from 'mongodb';
 
 import { Area } from '../../../Models/Area';
 import { Campaign } from '../../../Models/Campaign';
 import { log } from '../../../tools/log';
+import { checkParameters, hashPasword } from '../../../tools/utils';
 
+/**
+ * Change the script of a campaign
+ *
+ * @example
+ * body:{
+ *	"adminCode": string,
+ *	"newScript": string,
+ *	"area": mongoDBID,
+ *	"CampaignId": mongoDBID,
+ *	"allreadyHaseded": boolean
+ * }
+ *
+ * @throws {400} - Missing parameters
+ * @throws {400} - bad hash for admin code
+ * @throws {401} - Wrong admin code
+ * @throws {401} - Wrong campaign id
+ * @throws {401} - Wrong script id
+ * @throws {200} - OK
+ */
 export default async function changeScript(req: Request<any>, res: Response<any>) {
 	const ip = req.hostname;
 	if (
-		!req.body ||
-		typeof req.body.adminCode != 'string' ||
-		typeof req.body.newScript != 'string' ||
-		!ObjectId.isValid(req.body.area) ||
-		(req.body.CampaignId && !ObjectId.isValid(req.body.CampaignId))
-	) {
-		res.status(400).send({ message: 'Missing parameters', OK: false });
-		log(`Missing parameters from ` + ip, 'WARNING', __filename);
+		!checkParameters(
+			req.body,
+			res,
+			[
+				['adminCode', 'string'],
+				['newScript', 'string'],
+				['area', 'string'],
+				['CampaignId', 'string', true],
+				['allreadyHaseded', 'boolean', true]
+			],
+			__filename
+		)
+	)
 		return;
-	}
 
-	const area = await Area.findOne({ _id: { $eq: req.body.area }, adminPassword: { $eq: req.body.adminCode } });
+	const password = hashPasword(req.body.adminCode, req.body.allreadyHaseded, res);
+	if (!password) return;
+	const area = await Area.findOne({ _id: { $eq: req.body.area }, adminPassword: { $eq: password } });
 	if (!area) {
 		res.status(401).send({ message: 'Wrong admin code', OK: false });
 		log(`Wrong admin code from ${ip}`, 'WARNING', __filename);
@@ -45,7 +70,7 @@ export default async function changeScript(req: Request<any>, res: Response<any>
 		return;
 	}
 
-	const output = await Campaign.updateOne({ _id: { $eq: campaign._id } }, { $push: { script: req.body.newScript } });
+	const output = await Campaign.updateOne({ _id: { $eq: campaign._id } }, { script: req.body.newScript });
 	if (output.matchedCount != 1) {
 		res.status(400).send({ message: 'Campaign not found', OK: false });
 		log(`Campaign not found from ${ip}`, 'WARNING', __filename);
---
+++ tests/admin/campaign/changeScript.test.ts
@@ -0,0 +1,93 @@
+import dotenv from 'dotenv';
+import mongoose, { Types } from 'mongoose';
+import request from 'supertest';
+import app from '../../../index';
+import { Area } from '../../../Models/Area';
+import { Campaign } from '../../../Models/Campaign';
+
+dotenv.config({ path: '.env' });
+
+let areaId: mongoose.Types.ObjectId | undefined;
+let campaignId: mongoose.Types.ObjectId | undefined;
+
+const adminCode =
+	'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86'; //password
+
+beforeAll(async () => {
+	await mongoose.connect(process.env.URITEST ?? '');
+	await Area.deleteMany({});
+	await Campaign.deleteMany({});
+
+	areaId = (
+		await Area.create({
+			name: 'changeCampaignPasswordTest',
+			password: 'password',
+			campaignList: [],
+			adminPassword: adminCode
+		})
+	).id;
+
+	campaignId = (
+		await Campaign.create({
+			name: 'changeCampaignPasswordTest',
+			script: 'changeCampaignPasswordTest',
+			active: true,
+			area: areaId,
+			status: ['In progress', 'Finished'],
+			password: 'password'
+		})
+	).id;
+	Area.updateOne({ _id: areaId }, { $push: { campaignList: campaignId } });
+});
+
+afterAll(async () => {
+	await mongoose.connection.close();
+});
+
+describe('post on /api/admin/campaign/changeScript', () => {
+	it('should return 401 if the admin code is wrong', async () => {
+		const res = await request(app).post('/api/admin/campaign/changeScript').send({
+			adminCode: 'wrong',
+			newScript: 'newScript',
+			area: areaId
+		});
+		expect(res.status).toBe(401);
+		expect(res.body.message).toBe('Wrong admin code');
+	});
+
+	it('should return 401 if the campaign id is wrong', async () => {
+		const res = await request(app).post('/api/admin/campaign/changeScript').send({
+			adminCode,
+			newScript: 'newScript',
+			area: areaId,
+			allreadyHaseded: true,
+			CampaignId: new Types.ObjectId()
+		});
+		expect(res.status).toBe(401);
+		expect(res.body.message).toBe('Wrong campaign id');
+	});
+
+	it('should return 401 if the script is empty', async () => {
+		const res = await request(app).post('/api/admin/campaign/changeScript').send({
+			adminCode,
+			newScript: ' ',
+			area: areaId,
+			allreadyHaseded: true
+		});
+		expect(res.status).toBe(401);
+		expect(res.body.message).toBe('Wrong script id');
+	});
+
+	it('should return 200 if the script is changed', async () => {
+		const res = await request(app).post('/api/admin/campaign/changeScript').send({
+			adminCode,
+			newScript: 'newScript',
+			area: areaId,
+			allreadyHaseded: true
+		});
+		expect(res.status).toBe(200);
+		expect(res.body.message).toBe('OK');
+		const newScript = (await Campaign.findOne({ _id: campaignId }))?.script;
+		expect(newScript).toBe('newScript');
+	});
+});
