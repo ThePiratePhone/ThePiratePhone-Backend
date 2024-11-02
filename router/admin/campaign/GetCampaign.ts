5cfc900031f98c7ca8f31dac8f582072fdb0b852
unel <enee.prog@gmail.com>
2024-11-02 15:30:26 +0100

    add tests for getCampagn

--- router/admin/campaign/GetCampaign.ts
+++ router/admin/campaign/GetCampaign.ts
@@ -1,27 +1,48 @@
 import { Request, Response } from 'express';
-import { ObjectId } from 'mongodb';
 
 import { Area } from '../../../Models/Area';
 import { Campaign } from '../../../Models/Campaign';
 import { log } from '../../../tools/log';
+import { checkParameters, hashPasword } from '../../../tools/utils';
 
+/**
+ * get a campaign
+ *
+ * @example
+ * body:{
+ * 	"adminCode": string,
+ * 	"area": mongoDBID,
+ * 	"CampaignId": mongoDBID,
+ * 	"allreadyHaseded": boolean
+ * }
+ *
+ * @throws {400} - Missing parameters
+ * @throws {400} - bad hash for admin code
+ * @throws {401} - Wrong admin code
+ * @throws {404} - no campaign
+ * @throws {200} - OK
+ */
 export default async function getCampaign(req: Request<any>, res: Response<any>) {
 	const ip = req.hostname;
-	if (
-		!req.body ||
-		typeof req.body.adminCode != 'string' ||
-		!ObjectId.isValid(req.body.CampaignId) ||
-		!ObjectId.isValid(req.body.area)
-	) {
-		res.status(400).send({ message: 'Missing parameters', OK: false });
-		log(`Missing parameters from: ` + ip, 'WARNING', __filename);
-		return;
-	}
 
-	const area = await Area.findOne({ adminPassword: { $eq: req.body.adminCode }, _id: { $eq: req.body.area } }, [
-		'_id',
-		'name'
-	]);
+	if (
+		!checkParameters(
+			req.body,
+			res,
+			[
+				['adminCode', 'string'],
+				['CampaignId', 'string'],
+				['area', 'string'],
+				['allreadyHaseded', 'boolean', true]
+			],
+			__filename
+		)
+	)
+		return;
+
+	const password = hashPasword(req.body.adminCode, req.body.allreadyHaseded, res);
+	if (!password) return;
+	const area = await Area.findOne({ adminPassword: { $eq: password }, _id: { $eq: req.body.area } }, ['_id', 'name']);
 	if (!area) {
 		res.status(401).send({ message: 'Wrong admin code', OK: false });
 		log(`Wrong admin code from ` + ip, 'WARNING', __filename);
---
+++ tests/admin/campaign/getCampaign.test.ts
@@ -0,0 +1,91 @@
+import dotenv from 'dotenv';
+import mongoose, { Types } from 'mongoose';
+import request from 'supertest';
+import app from '../../../index';
+import { Area } from '../../../Models/Area';
+import { Campaign } from '../../../Models/Campaign';
+
+dotenv.config({ path: '.env' });
+
+let areaId: mongoose.Types.ObjectId | undefined;
+let CampaignId: mongoose.Types.ObjectId | undefined;
+
+const adminCode =
+	'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86'; //password
+
+beforeAll(async () => {
+	await mongoose.connect(process.env.URITEST ?? '');
+	await Area.deleteMany({});
+	await Campaign.deleteMany({});
+
+	areaId = (
+		await Area.create({
+			name: 'changeCampaignPasswordTest',
+			password: 'password',
+			campaignList: [],
+			adminPassword: adminCode
+		})
+	).id;
+
+	CampaignId = (
+		await Campaign.create({
+			name: 'changeCampaignPasswordTest',
+			script: 'changeCampaignPasswordTest',
+			active: true,
+			area: areaId,
+			status: ['In progress', 'Finished'],
+			password: 'password'
+		})
+	).id;
+	Area.updateOne({ _id: areaId }, { $push: { campaignList: CampaignId } });
+});
+
+afterAll(async () => {
+	await mongoose.connection.close();
+});
+
+describe(' post on /api/admin/campaign/getCampaign', () => {
+	it('should return 401 if the admin code is wrong', async () => {
+		const res = await request(app).post('/api/admin/campaign/getCampaign').send({
+			adminCode: 'wrong',
+			area: areaId,
+			CampaignId: areaId
+		});
+		expect(res.status).toBe(401);
+		expect(res.body.message).toBe('Wrong admin code');
+	});
+
+	it('should return 404 if the campaign id is wrong', async () => {
+		const res = await request(app).post('/api/admin/campaign/getCampaign').send({
+			adminCode,
+			area: areaId,
+			CampaignId: new Types.ObjectId(),
+			allreadyHaseded: true
+		});
+		expect(res.status).toBe(404);
+		expect(res.body.message).toBe('no campaign');
+	});
+
+	it('should return 401 if the area id is wrong', async () => {
+		const res = await request(app).post('/api/admin/campaign/getCampaign').send({
+			adminCode,
+			area: new Types.ObjectId(),
+			CampaignId: areaId,
+			allreadyHaseded: true
+		});
+		expect(res.status).toBe(401);
+		expect(res.body.message).toBe('Wrong admin code');
+	});
+
+	it('should return 200 if is correct', async () => {
+		const res = await request(app).post('/api/admin/campaign/getCampaign').send({
+			adminCode,
+			area: areaId,
+			CampaignId,
+			allreadyHaseded: true
+		});
+		expect(res.status).toBe(200);
+		expect(res.body.message).toBe('OK');
+		expect(res.body.data.active).toBe(true);
+	});
+});
